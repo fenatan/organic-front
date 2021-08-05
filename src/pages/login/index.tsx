403c75356c00dedf86d8db52dd0ae8cbf709e328
Felipe natan <felipenatan@novaandradina.org>
2021-08-05 14:35:36 -0400

    add base services

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,13 @@
 import { Heading, Box } from '@chakra-ui/react';
 
-export default function Login() {
+export default function Login({ env }) {
   return (
     <Box bg="primary.100" w="100%" p={4} color="white">
-      <Heading size="lg">
-        {process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/graphql'}
-      </Heading>
+      <Heading size="lg">{env || 'http://localhost:3000/graphql'}</Heading>
     </Box>
   );
 }
+
+Login.getInitialProps = async () => {
+  return { env: process.env.NEXT_PUBLIC_API_URL };
+};
