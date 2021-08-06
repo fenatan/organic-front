344d615f920ee08e43c71e27ba94f7d39e5d38d5
Mateus Fernandes <mateusfernandes12@live.com>
2021-08-05 22:12:25 -0300

    habilita a autenticacao no servico

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
